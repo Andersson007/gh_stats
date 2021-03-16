94f2fc0e654ac33b6015ed1982f51a4d0f964ffe
Andrew Klychkov <aklychko@redhat.com>
2021-03-16 17:02:22 +0100

    Add get_repos_from_db and add_repo_to_db functions

--- gh_stats_collector.py
+++ gh_stats_collector.py
@@ -8,6 +8,8 @@ from github import (
     Repository,
 )
 
+from psycopg2.extensions import cursor as PgCursor
+
 from utils.connection import connect_to_db
 
 __VERSION__ = '0.1'
@@ -30,14 +32,31 @@ def get_cli_args():
                         help='GitHub repository name or comma-separated list of names',
                         metavar='REPO_NAME')
 
-    # TODO: Add DB connection parameters
-    #
+    # DB connection related parameters
+    parser.add_argument('-d', '--database', dest='database', required=True,
+                        help='Database name to connect to', metavar='DBNAME')
+
+    parser.add_argument('-u', '--user', dest='user', required=True,
+                        help='Database user to log in with', metavar='DBUSER')
+
+    parser.add_argument('-p', '--password', dest='password', required=True,
+                        help='Database password', metavar='DBPASS')
 
     return parser.parse_args()
 
 
 def extract_repos(cli_arg: str) -> list:
     return [repo.strip() for repo in cli_arg.split(',')]
+
+
+def get_repos_from_db(cursor: PgCursor) -> list:
+    repo_in_db = []
+
+    return repo_in_db
+
+
+def add_repo_to_db(cursor: PgCursor, repo_name: str):
+    pass
 
 
 def handle_tags(repo: Repository):
@@ -64,6 +83,13 @@ def main():
     # Get command-line arguments
     cli_args = get_cli_args()
 
+    # Connect to database
+    conn, cursor = connect_to_db(database=cli_args.database,
+                                 user=cli_args.user,
+                                 password=cli_args.password)
+
+    repos_in_db = get_repos_from_db(cursor)
+
     # Create github object and set access token
     gh = Github(cli_args.token)
 
@@ -75,12 +101,16 @@ def main():
     # Get orgs
     gh_org = gh.get_organization(cli_args.org)
 
-    # Get repos and do main job
+    # Get repos from GH and do main job
     for repo in gh_org.get_repos():
 
         # Skip what we don't need
         if repos_needed and repo.name not in repos_needed:
             continue
+
+        # If we don't have it now, add repo to DB
+        if repo.name not in repos_in_db:
+            add_repo_to_db(cursor, repo)
 
         # Handle tags
         handle_tags(repo)
@@ -95,6 +125,9 @@ def main():
 
     # TODO: Remove this debug print after all
     #print(repo_list)
+
+    # DB close connection
+    conn.close()
 
 
 if __name__ == '__main__':
--- utils/connection.py
+++ utils/connection.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+import sys
+
 from psycopg2 import connect
 from psycopg2.extensions import cursor
+from psycopg2.extras import DictCursor
 
 
 def connect_to_db(database: str, user: str, password: str) -> (connect, cursor):
@@ -13,12 +16,14 @@ def connect_to_db(database: str, user: str, password: str) -> (connect, cursor):
     # that you run PostgreSQL locally.
     # Be sure, you configure access properly
 
+    conn = None
     try:
         conn = connect(host='localhost',
                        database=database,
                        user=user,
                        password=password)
     except Exception as e:
-        print('Unable to connect to the database: %s' % e)
+        print('Unable to connect to the database: %s' % e, file=sys.stderr)
+        sys.exit(1)
 
-    return conn, conn.cursor()
+    return conn, conn.cursor(cursor_factory=DictCursor)
