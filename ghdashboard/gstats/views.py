0c50b3153a53a177841dcc9094a6ae63057f77d0
Andrew Klychkov <aklychko@redhat.com>
2021-04-29 15:43:36 +0200

    Add test repo table rendering

--- ghdashboard/gstats/views.py
+++ ghdashboard/gstats/views.py
@@ -1,10 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Repos
 
 
 def test_view(request):
     """Test view"""
-    query_result = [repo.name for repo in Repos.objects.all()]
+    repo_list = sorted([repo.name for repo in Repos.objects.all() if repo.name != '.github'])
 
-    return HttpResponse(query_result)
+    repo_num = len(repo_list)
+
+    context = {
+        'repo_list': repo_list,
+        'repo_num': repo_num,
+    }
+
+    return render(request, 'gstats/index.html', context)
