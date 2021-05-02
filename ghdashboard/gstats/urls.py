bb8b0e95f56278b767239e75bfe636f66e1f85b9
Andrew Klychkov <aklychko@redhat.com>
2021-05-02 08:57:42 +0200

    Add repos-branches.html

--- ghdashboard/gstats/urls.py
+++ ghdashboard/gstats/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('repos.html', views.repos, name='repos'),
+    path('repos-branches.html', views.repos_and_branches, name='repos-branches'),
 ]
--- ghdashboard/gstats/views.py
+++ ghdashboard/gstats/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
-from .models import Repos
+from .models import Repos, Branches
+
 
 def index(request):
     return render(request, 'gstats/index.html', {})
 
+
 def repos(request):
-    """Repo list view"""
-    repo_list = sorted([repo.name for repo in Repos.objects.all() if repo.name != '.github'])
+    """Repo list view."""
+    repos = Repos.objects.raw('SELECT id, name FROM repos')
+    repo_list = sorted([repo.name for repo in repos])
 
     repo_num = len(repo_list)
 
@@ -17,3 +20,27 @@ def repos(request):
     }
 
     return render(request, 'gstats/repos.html', context)
+
+
+def repos_and_branches(request):
+    """Repos and their branches."""
+    repos = Repos.objects.raw('SELECT id, name FROM repos')
+
+    query = ('SELECT b.id, b.name FROM branches AS b '
+             'LEFT JOIN repos AS r ON r.id = b.repo_id '
+             'WHERE r.name = %s ORDER BY b.name')
+
+    repos_and_branches = []
+    for repo in repos:
+        branches = Branches.objects.raw(query, (repo.name,))
+        repos_and_branches.append([repo.name, [b.name for b in branches]])
+
+    repos_and_branches.sort()
+    repo_num = len(repos)
+
+    context = {
+        'repos_and_branches': repos_and_branches,
+        'repo_num': repo_num,
+    }
+
+    return render(request, 'gstats/repos-branches.html', context)
